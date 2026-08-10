ccb6ac5c2bdbe7ae2da790681df1781010b100b6
Alexsaydo <andreev.bu@gmail.com>
2026-08-11 01:44:21 +0300

    Update PWA cache for cable selection

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='wiring-v5';
+const CACHE='wiring-v6';
 const ASSETS=['./','./index.html','./app.js','./manifest.webmanifest','./icons/icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x)))));self.clients.claim()});
